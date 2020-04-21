29f6e85598cef723af96a78f695d87c15406826b
user <roy.barak.m@gmail.com>
2020-04-21 17:55:11 +0300

    import-flux-config

--- Deploy.sh
+++ Deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 CLUSTER_NAME=echo-cluster
-terraform init
-terraform apply -auto-approve -var cluster_name=$CLUSTER_NAME -var project=$GCP_PROJECT
 
-gcloud container clusters get-credentials $CLUSTER_NAME --region us-central1
+terraform init
+terraform apply -auto-approve \
+-var cluster_name=$CLUSTER_NAME \
+-var project=$GCP_PROJECT
+
+gcloud container clusters get-credentials $CLUSTER_NAME \
+--region us-central1
 
 kubectl apply -f k8/namespaces
 kubectl apply -f k8/secrets/prod.yaml -n production
@@ -16,7 +20,7 @@ helm install mongo k8/helm/mongodb -n db
 helm install prometheus k8/helm/prometheus-operator -n monitoring
 helm install es-op k8/helm/elasticsearch-operator -n loggin
 helm install efk k8/helm/efk -n loggin
-helm install -i flux k8/helm/flux -n fluxcd
+helm install flux k8/helm/flux -n fluxcd
 helm install helm-operator k8/helm/helm-operator -n fluxcd
 
 fluxctl identity --k8s-fwd-ns fluxcd
