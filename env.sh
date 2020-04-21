8f260960abd74d5eaae6f229fa4e5d9c90699108
user <roy.barak.m@gmail.com>
2020-04-21 21:53:18 +0300

    releses-fix

--- env.sh
+++ env.sh
@@ -3,4 +3,3 @@
 export GCP_PROJECT=echo-k8s-project
 export GCP_ACCOUNT_NAME=rainrobot
 export GCP_ACCOUNT_ADDRESS=$GCP_ACCOUNT_NAME@$GCP_PROJECT.iam.gserviceaccount.com
-#export TF_VAR_project=$GCP_PROJECT
