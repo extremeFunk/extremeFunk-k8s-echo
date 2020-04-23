5963bd3d010ff4cbe19ab28a2a7da9258b7098fe
user <roy.barak.m@gmail.com>
2020-04-23 20:38:19 +0300

    fix-dev-release

--- Deploy.sh
+++ Deploy.sh
@@ -32,13 +32,12 @@ helm install mongo k8/helm/mongodb -n db \
 helm install prometheus k8/helm/prometheus-operator -n monitoring
 helm install es-op k8/helm/elasticsearch-operator -n loggin
 helm install efk k8/helm/efk -n loggin
+helm install jenkins k8/helm/jenkins -n jenkins
 helm install flux k8/helm/flux -n fluxcd --wait
 helm install helm-operator k8/helm/helm-operator -n fluxcd
 
 fluxctl identity --k8s-fwd-ns fluxcd
-
 echo "upload this key to git with name = flux-key"
 echo "add dns name - press any key"
 read userin
-
 fluxctl sync --k8s-fwd-ns fluxcd
